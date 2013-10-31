b3341408d1f3f618276f88edf3fd1d44bf815f6d
EdgarSalvat <edgar.salvat@gmail.com>
2013-10-31 21:12:09 +0100

    Creació calculadora cientifica

--- CalculadoraGit/src/CalculadoraGit.java
+++ CalculadoraGit/src/CalculadoraGit.java
@@ -3,7 +3,7 @@ public class CalculadoraGit {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("Soc una CalculadoraGit simple");
+		System.out.println("Soc una CalculadoraGit cientifica");
 	}
 
 }
